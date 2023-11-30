e793ab4ebf26d9d203f90f5a90ecae1ee969be71
Friskes <friskestv@gmail.com>
2023-11-30 20:09:09 +0300

    added the ability to run on linux

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## Django autoreload adapting for automatic reloading of celery and its modules when modify files in online mode
+## Django command created to run `celery (worker, beat, flower)` with one command and to automatically reboot servers after changing files
 
 ### Installing the program
 
@@ -24,20 +24,41 @@ Your Project Folder
 └── other folders..
 ```
 
+### Dependencies
+- python-dotenv
+- psutil
+
 ### Working with the program
 
-#### Command to run the program: ```python manage.py runcelery <project_name>```
+#### Command to run the program:
+```
+python manage.py runcelery
+```
 
-The only mandatory argument is the name of the project.
 Arguments can be passed in any order, it doesn't matter.
 
 #### Positional Arguments:
-`<project_name>`
+`<celery app name>`
+or
+`<"full command to run celery worker">`
 
 #### Optional Arguments:
-`-h` Show help message.<br/>
-`-b` Excludes the beat server at startup.<br/>
-`-f` Excludes the flower server at startup.<br/>
-`-d` Displays information about successful/unsuccessful completion of processes.
+`-h` or `--help` Show help message.<br/>
+`-b` or `--beat` Excludes the beat server at startup.<br/>
+`-f` or `--flower` Excludes the flower server at startup.<br/>
+`-d` or `--debug` Displays information about successful/unsuccessful completion of processes.<br/>
+`-ll` or `--loglevel` Defines the logging level for celery worker/beat
+`-lf` or `--logfile` Redirects the output to the console by default to a log file for celery worker/beat
 
-##### To stopped program pressing the keyboard shortcut CTRL+C
+##### To stopped program pressing the keyboard shortcut `CTRL+C`
+
+### Examples of Commands
+The command enclosed in quotation marks gets into the positional arguments and replaces the command to run the default celery worker.
+The `--beat` `--flower` commands fall into the optional arguments and turn off the beat and flower of the server.
+```shell script
+python manage.py runcelery "celery --app=${CELERY_APP} worker -E \
+--statedb=/var/run/celery/worker-example@%h.state \
+--hostname=worker-example@%h --uid=nobody --gid=nogroup \
+--loglevel=INFO \
+--logfile=/var/log/celery/worker-example.log" --beat --flower
+```
--- management/commands/runcelery.py
+++ management/commands/runcelery.py
@@ -1,12 +1,19 @@
+from __future__ import annotations
 import os
+import sys
 import shlex
+import signal
 import subprocess
 
-from . import _localization as L
-
-from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.utils import autoreload
+from django.conf import settings
+
+import psutil
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
+from . import _localization as L
 
 
 # https://habr.com/ru/articles/415049/
@@ -14,80 +21,65 @@ from django.utils import autoreload
 # https://simpleisbetterthancomplex.com/tutorial/2018/08/27/how-to-create-custom-django-management-commands.html
 # https://docs.djangoproject.com/en/4.2/howto/custom-management-commands/
 
-BASE_DIR = str(settings.BASE_DIR).replace('\\', '/') + '/'
-
 
 class Command(BaseCommand):
+    """
+    https://github.com/Friskes/celery_starter
+    """
+    help = L.command_help + " https://github.com/Friskes/celery_starter"
 
-    help=L.command_help
+    BASE_DIR = str(settings.BASE_DIR) + os.sep
 
-    def handle(self, *args, **options):
-        """Срабатывает при первом запуске и изменении любого файла проекта."""
+    def read_pid_file(self, file_name: str):
+        """
+        Считывает pid из файла, убивает связанный с ним процесс и удаляет этот файл.
+        Файл создается автоматически средствами celery beat
+        """
+        if file_name in os.listdir(self.BASE_DIR):
+            with open(file_name, 'r') as file:
 
-        self.arguments = args
-        self.options = options
-        self.project_name = args[0] if args else 'FriskesSite'
+                for line in file.readlines():
+                    try:
+                        os.kill(int(line), signal.SIGTERM)
+                    except OSError:
+                        pass
+                    # try:
+                    #     if self.options['debug']:
+                    #         subprocess.check_call("TASKKILL /F /PID {pid} /T".format(pid=int(line)))
+                    #     else:
+                    #         subprocess.check_call(
+                    #             "TASKKILL /F /PID {pid} /T".format(pid=int(line)),
+                    #             stdin=subprocess.PIPE, stderr=subprocess.PIPE, stdout=subprocess.PIPE
+                    #         )
+                    # except subprocess.CalledProcessError:
+                    #     pass
 
-        beat = 'beat ' if not self.options['beat'] else ''
-        flower = 'flower ' if not self.options['flower'] else ''
+            os.remove(self.BASE_DIR + file_name)
 
-        if self.options['debug']:
-            msg = L.start_msg % (self.project_name, beat, flower) + L.debug_msg
-        else:
-            msg = L.start_msg % (self.project_name, beat, flower)
+    def run_celery_win(self):
+        """
+        Запускает celery[worker/beat/flower] на Windows
+        с выводом логов в одну консоль, для локальной разработки.
+        """
+        #                                                             -P eventlet # -P threads # -P gevent
+        celeryworker_cmd = f"celery -A {self.celery_app} worker -E -l {self.options['loglevel']} -P solo"
+        if self.options['logfile']:
+            celeryworker_cmd += f" --logfile={self.options['logfile']}"
 
-        colored_msg = (self.style.SUCCESS(msg.split('[')[0].split('(')[0] + '(')
-                       + self.style.MIGRATE_HEADING(msg.split('[')[0].split('(')[1].split(')')[0])
-                       + self.style.SUCCESS(') [')
-                       + self.style.HTTP_SERVER_ERROR(msg.split('[')[1].split(']')[0]))
-        if self.options['debug']:
-            colored_msg += (self.style.SUCCESS(']' + msg.split('[')[1].split(']')[1].split('DEBUG')[0])
-                            + self.style.WARNING('DEBUG')
-                            + self.style.SUCCESS(msg.split('[')[1].split(']')[1].split('DEBUG')[1]))
-        else:
-            colored_msg += self.style.SUCCESS(']' + msg.split('[')[1].split(']')[1])
-
-        self.stdout.write(colored_msg)
-
-        autoreload.run_with_reloader(self.restart_celery)
-
-
-    def add_arguments(self, parser):
-        """Добавляет парсер аргументов которые попадут в options метода handle."""
-
-        parser.add_argument(nargs='*', type=str, dest='args', help=L.proj_name_help)
-        parser.add_argument('-b', '--beat', action='store_true', default=False, help=L.beat_help)
-        parser.add_argument('-f', '--flower', action='store_true', default=False, help=L.flower_help)
-        parser.add_argument('-d', '--debug', action='store_true', default=False, help=L.debug_help)
-
-
-    def restart_celery(self):
-        """Убивает все существующие celery процессы и заного запускает их."""
-
-        if self.options['debug']:
-            subprocess.call(shlex.split("TASKKILL /F /T /IM celery.exe"))
-            # subprocess.run(["celery", "-A", self.project_name, "control", "shutdown"])
-        else:
-            subprocess.call(
-                shlex.split("TASKKILL /F /T /IM celery.exe"),
-                stdin=subprocess.PIPE, stderr=subprocess.PIPE, stdout=subprocess.PIPE
-            )
-            # subprocess.run(
-            #     ["celery", "-A", self.project_name, "control", "shutdown"],
-            #     stdin=subprocess.PIPE, stderr=subprocess.PIPE, stdout=subprocess.PIPE
-            # )
-        self.read_pid_file("celerybeat.pid")
-
+        # subprocess.run(
+        #     self.command if self.command else shlex.split(celeryworker_cmd)
+        # )
         subprocess.Popen(
-            shlex.split(f"celery -A {self.project_name} worker -l info -P eventlet"), # -P solo # -P threads # -P gevent
+            self.command if self.command else shlex.split(celeryworker_cmd),
             stdin=subprocess.PIPE, stderr=subprocess.STDOUT
         )
 
         if not self.options['beat']:
+            self.read_pid_file("celerybeat.pid")
+            celerybeat_cmd = f"celery -A {self.celery_app} beat -l {self.options['loglevel']} --pidfile=celerybeat.pid"
             # https://docs.celeryq.dev/en/latest/userguide/periodic-tasks.html#using-custom-scheduler-classes
-            celerybeat_cmd = f"celery -A {self.project_name} beat -l info --pidfile=celerybeat.pid"
-            # celerybeat_cmd += " " + "--scheduler django_celery_beat.schedulers:DatabaseScheduler"
-            # celerybeat_cmd += " " + "-S django"
+            # celerybeat_cmd += " --scheduler django_celery_beat.schedulers:DatabaseScheduler"
+            # celerybeat_cmd += " -S django"
             subprocess.Popen(
                 shlex.split(celerybeat_cmd),
                 stdin=subprocess.PIPE, stderr=subprocess.STDOUT
@@ -95,33 +87,135 @@ class Command(BaseCommand):
 
         if not self.options['flower']:
             subprocess.Popen(
-                shlex.split(f"celery -A {self.project_name} flower --url_prefix=flower"),
+                shlex.split(f"celery -A {self.celery_app} flower --url_prefix=flower"),
                 stdin=subprocess.PIPE, stderr=subprocess.PIPE, stdout=subprocess.PIPE
             )
 
-
-    def read_pid_file(self, file_name: str):
-        """Считывает pid из файла и отправляет его в task_kill_by_pid для удаления процесса."""
-
-        if file_name in os.listdir(BASE_DIR):
-            with open(file_name, 'r') as file:
-
-                for line in file.readlines():
-                    self.task_kill_by_pid(int(line))
-
-            os.remove(BASE_DIR + file_name)
-
-
-    def task_kill_by_pid(self, pid: int):
-        """Удаляет процесс используя pid."""
-
-        try:
-            if self.options['debug']:
-                subprocess.check_call("TASKKILL /F /PID {pid} /T".format(pid=pid))
+    def get_main_process(self) -> set[int]:
+        """
+        Возвращает список pid активных celery процессов (если они есть).
+        """
+        processes_pids = set()
+        for process in psutil.process_iter():
+            ppid = process.ppid()
+            if ppid == 0:
+                continue
+            try:
+                parent = psutil.Process(ppid)
+            except psutil.NoSuchProcess:
+                pass
             else:
-                subprocess.check_call(
-                    "TASKKILL /F /PID {pid} /T".format(pid=pid),
+                if process.name() == 'python.exe' and parent.name() == 'celery.exe':
+                    processes_pids.add(process.pid)
+                    processes_pids.add(parent.pid)
+            return processes_pids
+
+    def kill_celery_processes(self, processes_pids: set[int]):
+        """
+        Убивает celery процесс(ы).
+        """
+        if sys.platform == "win32":
+            if self.options['debug']:
+                subprocess.call(shlex.split("TASKKILL /F /T /IM celery.exe"))
+                # subprocess.run(["celery", "-A", self.project_name, "control", "shutdown"])
+            else:
+                subprocess.call(
+                    shlex.split("TASKKILL /F /T /IM celery.exe"),
                     stdin=subprocess.PIPE, stderr=subprocess.PIPE, stdout=subprocess.PIPE
                 )
-        except subprocess.CalledProcessError:
-            pass
+                # subprocess.run(
+                #     ["celery", "-A", self.project_name, "control", "shutdown"],
+                #     stdin=subprocess.PIPE, stderr=subprocess.PIPE, stdout=subprocess.PIPE
+                # )
+        else:
+            for pid in processes_pids:
+                os.kill(pid, signal.SIGTERM)
+
+    def reload_celery(self):
+        """
+        Убивает процесс(ы) celery если он(и) есть и заного запускает celery.
+        """
+        self.kill_celery_processes(self.get_main_process())
+
+        if sys.platform == "win32":
+            self.run_celery_win()
+        else:
+            subprocess.run(shlex.split(self.command))
+
+    def get_celery_app(self) -> tuple[str, str | None]:
+        """
+        Возвращает название приложения celery (если находит)
+        и команду для запуска worker (если она была передана).
+        """
+        if self.args:
+            cmd = self.args[0].split()
+            cmd_len = len(cmd)
+            if cmd_len == 1:
+                return cmd[0], None
+            else:
+                for i in range(cmd_len):
+                    if cmd[i] == '-A':
+                        return cmd[i+1].strip(), self.args[0]
+
+                    if cmd[i].startswith('--app'):
+                        return cmd[i].split('=')[-1].strip(), self.args[0]
+        else:
+            if os.environ.get('CELERY_APP'):
+                return os.environ.get('CELERY_APP'), None
+
+            settings_filename = 'settings.py'
+            for root, dirs, files in os.walk(self.BASE_DIR):
+                if settings_filename in files:
+                    return root.rsplit('/', 1)[-1], None
+
+        raise ValueError(L.celery_app_not_found)
+
+    def add_arguments(self, parser):
+        """
+        Добавляет парсер для аргументов которые попадают в options метода handle.
+        """
+        parser.add_argument(nargs='*', type=str, dest='args', help=L.proj_name_help)
+        parser.add_argument('-b', '--beat', action='store_true', default=False, help=L.beat_help)
+        parser.add_argument('-f', '--flower', action='store_true', default=False, help=L.flower_help)
+        parser.add_argument('-d', '--debug', action='store_true', default=False, help=L.debug_help)
+        parser.add_argument('-ll', '--loglevel', default='INFO', type=str, help=L.log_level)
+        parser.add_argument('-lf', '--logfile', default='', type=str, help=L.log_file)
+
+    def handle(self, *args, **options):
+        """
+        Срабатывает при первом запуске и изменении любого файла проекта.
+        """
+        self.args: tuple[str] = args
+        self.options = options
+        self.celery_app, self.command = self.get_celery_app()
+
+        beat = 'beat ' if not self.options['beat'] else ''
+        flower = 'flower ' if not self.options['flower'] else ''
+
+        defails = f"loglevel={self.options['loglevel']}"
+        if self.options['logfile']:
+            defails += f" logfile={self.options['logfile']}"
+
+        if self.options['debug']:
+            msg = L.start_msg % (self.celery_app, beat, flower, defails) + L.debug_msg
+        else:
+            msg = L.start_msg % (self.celery_app, beat, flower, defails)
+
+        colored_msg = (
+            self.style.SUCCESS(msg.split('[')[0].split('(')[0] + '(')
+            + self.style.MIGRATE_HEADING(msg.split('[')[0].split('(')[1].split(')')[0])
+            + self.style.SUCCESS(') [')
+            + self.style.HTTP_SERVER_ERROR(msg.split('[')[1].split(']')[0])
+        )
+        if self.options['debug']:
+            colored_msg += (
+                self.style.SUCCESS(']' + msg.split('[')[1].split(']')[1].split('DEBUG')[0])
+                + self.style.WARNING('DEBUG')
+                + self.style.SUCCESS(msg.split('[')[1].split(']')[1].split('DEBUG')[1])
+            )
+        else:
+            colored_msg += self.style.SUCCESS(']' + msg.split('[')[1].split(']')[1])
+
+        self.stdout.write(colored_msg)
+
+        autoreload.run_with_reloader(self.reload_celery)
